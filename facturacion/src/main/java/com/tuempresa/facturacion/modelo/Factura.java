d11cdbcadd95b82e887f82cb1e4ec63697882a96
Illdyr <nrlagunav@uamv.edu.ni>
2025-11-04 22:03:23 -0600

    se agragan cambios en la interfaz de cliente de la entidad factura

--- facturacion/src/main/java/com/tuempresa/facturacion/modelo/Factura.java
+++ facturacion/src/main/java/com/tuempresa/facturacion/modelo/Factura.java
@@ -10,7 +10,15 @@ import org.openxava.annotations.*;
 import org.openxava.calculators.*;
 import lombok.*;
 
+
+
 @Entity @Getter @Setter
+
+@View(members= // Esta vista no tiene nombre, por tanto será la vista usada por defecto
+        "anyo, numero, fecha;" + // Separados por coma significa en la misma línea
+                "cliente;" + // Punto y coma significa nueva línea
+                "detalles;" +
+                "observaciones")
 public class Factura {
 
     @Id
@@ -38,7 +46,8 @@ public class Factura {
     @TextArea
     String observaciones;
 
-    @ManyToOne(fetch=FetchType.LAZY, optional=false) // El cliente es obligatorio
+    @ManyToOne(fetch=FetchType.LAZY, optional=false)
+    @ReferenceView("Simple") // La vista llamada 'Simple' se usará para visualizar esta referencia
     Cliente cliente;
 
     @ElementCollection
